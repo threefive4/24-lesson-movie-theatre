401ef35491f3c51ff40715ca3d9753c5e4096afd
threefive4 <120577545+threefive4@users.noreply.github.com>
2023-04-24 20:23:08 +0300

    Add files via upload

---
+++ src/ua/lviv/lgs/Days.java
@@ -0,0 +1,11 @@
+package ua.lviv.lgs;
+
+public enum Days {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+}
---
+++ src/ua/lviv/lgs/Schedule.java
@@ -0,0 +1,23 @@
+package ua.lviv.lgs;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Schedule {
+    private Set<Seance> seances = new TreeSet<>();
+
+    public void addSeance(Seance seance) {
+        seances.add(seance);
+    }
+    public void removeSeance(Seance seance){
+        seances.remove(seance);
+    }
+
+    public Set<Seance> getSeances() {
+        return seances;
+    }
+
+    public void setSeances(Set<Seance> seances) {
+        this.seances = seances;
+    }
+}
